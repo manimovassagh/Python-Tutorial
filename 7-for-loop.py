e2b928d75e9ce7f7207927be10ccafc46c8010e2
Mani Movassagh <mani.mgh@gmail.com>
2023-11-06 00:57:02 +0100

    python if

--- 7-for-loop.py
+++ 7-for-loop.py
@@ -1,13 +1,12 @@
-cities = ["frankfurt","tehran","Dubai"]
+import os
+cities = ["frankfurt", "tehran", "Dubai"]
 for city in cities:
     print(city)
     if city == "tehran":
         break
-    
-    
+
+
 for x in range(10):
     print("This is the number ", x)
-
-
-
-
+m = os.mkdir("This is made by python")
+print(m)
