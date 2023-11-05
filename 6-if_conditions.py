34627ab1500feac0ab2239cc809338a6bfb6c930
Mani Movassagh <mani.mgh@gmail.com>
2023-11-05 16:26:57 +0100

    this is commit

--- 6-if_conditions.py
+++ 6-if_conditions.py
@@ -1,1 +1,15 @@
-name 
+if 22 > 4:
+    print("22 is greater than 4") 
+
+
+if 2>4:
+    print("this is greater")
+elif 4 > 2:
+    print("this is greater")
+else:
+    print("this is greater")
+    
+a = 22
+b = 2
+if a != b:
+    print("They are not equal")
---
+++ 7-for-loop.py
@@ -0,0 +1,6 @@
+for x in range(10):
+    print("This is the number ", x)
+
+
+
+
