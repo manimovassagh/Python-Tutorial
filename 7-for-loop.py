22726c1a6f8f897691b3fba1c297e7420a77f0c7
Mani Movassagh <mani.mgh@gmail.com>
2023-11-05 16:31:23 +0100

    loop

--- 7-for-loop.py
+++ 7-for-loop.py
@@ -1,3 +1,10 @@
+cities = ["frankfurt","tehran","Dubai"]
+for city in cities:
+    print(city)
+    if city == "tehran":
+        break
+    
+    
 for x in range(10):
     print("This is the number ", x)
 
