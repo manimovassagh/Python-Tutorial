8b02c44b93cc5f04f1af213e8d5c7ca0c56eb0e4
Mani Movassagh <mani.mgh@gmail.com>
2023-11-07 01:42:45 +0100

    list items

--- 2-list.py
+++ 2-list.py
@@ -1,6 +1,12 @@
-my_list = ["Tehran","Frankfurt","Madrid","Paris"]
+my_list = ["Tehran", "Frankfurt", "Madrid", "Paris"]
 # my_list.append("London")
 # my_list.pop()
 # my_list.remove("Madrid")
 print(my_list[:3])
+my_list.sort()
+print(my_list)
+t = my_list.index("Tehran", 0, 4)
+print(t)
+p = my_list.count("Tehran")
+print(p)
 
