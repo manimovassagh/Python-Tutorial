70e1bcbb30e2befc2e5d88dfab6a1a52e938fd69
Mani Movassagh <mani.mgh@gmail.com>
2023-11-16 16:26:55 +0100

    classes2

---
+++ 15-inheritance.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, name, age):
+      self.name = name
+      self.age = age
+    def login(self):
+        print("User logged in")
+    def logout(self):
+        print("User logged out")
