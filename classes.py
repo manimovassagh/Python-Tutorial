f2a0a1342800e1c99aec9135757c32c90456d93d
Mani Movassagh <mani.mgh@gmail.com>
2023-11-16 14:59:28 +0100

    classes

---
+++ classes.py
@@ -0,0 +1,9 @@
+class User:
+    name = "mani"
+    email = "test@gmail.com"
+    def login(self):
+        print("logged in")
+    def logout(self):
+        print("logged out")
+   
+user_one = User()
