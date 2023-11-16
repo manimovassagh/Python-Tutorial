751d2110986f32b74e5238c3137b0318b9173f00
Mani Movassagh <mani.mgh@gmail.com>
2023-11-16 22:31:22 +0100

    class

--- 15-inheritance.py
+++ 15-inheritance.py
@@ -2,7 +2,20 @@ class User:
     def __init__(self, name, age):
       self.name = name
       self.age = age
+    def __str__(self):
+        return f"User: {self.name}, {self.age} years old"
     def login(self):
-        print("User logged in")
-    def logout(self):
-        print("User logged out")
+        print(f"{self.name} logged in")
+
+class Admin(User):
+          def __init__(self, name, age,is_admin):
+              super().__init__(name, age)
+              self.is_admin = is_admin
+          def login(self):
+              print(f"{self.name} logged in as admin")
+
+admin = Admin("John", 25, True)
+admin.login()
+user = User("Mani", 30)
+user.login()
+   
