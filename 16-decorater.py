673cb479d11dff8e32849deeb46b6f09c663b03e
Mani Movassagh <mani.mgh@gmail.com>
2023-11-18 14:58:54 +0100

    decorater

--- 16-decorater.py
+++ 16-decorater.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 def logger(function):
     def wrapper():
-        print("This is a logger decoraer")
+        print(datetime.now())
         function()
+        print(datetime.now())
     return wrapper
 
 
